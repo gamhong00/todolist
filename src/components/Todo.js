6bba5e768d395ef4d048f369be1e13653bd6db0d
Your Name <you@example.com>
2024-03-20 10:01:32 +0700

    push code

---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+export const todo = ({ task }) => {
+    return (
+        <div className='Todo'>
+            <p>{task.task}</p>
+            <div>
+                <FontAwesomeIcon icon={faPenToSquare} />
+                <FontAwesomeIcon icon={faTrash} />
+            </div>
+        </div>
+    )
+}
---
+++ src/components/TodoWrapper.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import { TodoForm } from './TodoForm';
+import { v4 as uuidv4 } from 'uuid';
+import { Todo } from './Todo';
+uuidv4();
+
+const TodoWrapper = () => {
+    const [todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+        setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+        // console.log(todos);
+    }
+
+    useEffect(() => {
+        console.log(todos);
+    }, [todos]);
+
+    return (
+        <div className='TodoWrapper'>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo, index) => (
+                <Todo task={todo} key={index} />
+            ))}
+            <Todo />
+        </div>
+    )
+}
+
+export default TodoWrapper;
